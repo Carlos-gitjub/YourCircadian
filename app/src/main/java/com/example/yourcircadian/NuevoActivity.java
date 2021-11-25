b93d8a1aa2c5ea0d56fc7b355161ab92247b3729
Carlos Anton <cantonplk@gmail.com>
2021-11-25 15:09:23 +0000

    updates

--- app/src/main/java/com/example/yourcircadian/NuevoActivity.java
+++ app/src/main/java/com/example/yourcircadian/NuevoActivity.java
@@ -3,8 +3,11 @@ package com.example.yourcircadian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.yourcircadian.db.DbContactos;
 
 public class NuevoActivity extends AppCompatActivity {
 
@@ -21,5 +24,12 @@ public class NuevoActivity extends AppCompatActivity {
         txtCorreoElectronico= findViewById(R.id.txtCorreoElectronico);
         btnGuarda = findViewById(R.id.btnGuarda);
 
+        btnGuarda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbContactos dbContactos = new DbContactos(NuevoActivity.this);
+                dbContactos.insertarContacto(txtNombre.getText().toString(), txtTelefono.getText().toString(), txtCorreoElectronico.getText().toString());
+            }
+        });
     }
 }
