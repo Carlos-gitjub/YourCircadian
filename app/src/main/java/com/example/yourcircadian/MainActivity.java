ff3f4b055399de04570a60c041502cb5b0992aec
Carlos Anton <cantonplk@gmail.com>
2021-11-25 16:02:23 +0000

    updates

--- app/src/main/java/com/example/yourcircadian/MainActivity.java
+++ app/src/main/java/com/example/yourcircadian/MainActivity.java
@@ -21,18 +21,15 @@ public class MainActivity extends AppCompatActivity {
         btnCrear = findViewById(R.id.btnCrear);
 
         //Acción cuando hacemos click
-        btnCrear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DbHelper dbHelper = new DbHelper(MainActivity.this);
-                SQLiteDatabase db = dbHelper.getWritableDatabase();  //para escribir en nuestra DB
-                if(db != null){
-                    Toast.makeText(MainActivity.this, "BASE DE DATOS CREADA", Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(MainActivity.this, "ERROR AL CREAR BASE DE DATOS", Toast.LENGTH_LONG).show();
-                }
-
+        btnCrear.setOnClickListener((view) -> {
+            DbHelper dbHelper = new DbHelper(MainActivity.this);
+            SQLiteDatabase db = dbHelper.getWritableDatabase();  //para escribir en nuestra DB
+            if(db != null){
+                Toast.makeText(MainActivity.this, "BASE DE DATOS CREADA", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(MainActivity.this, "ERROR AL CREAR BASE DE DATOS", Toast.LENGTH_LONG).show();
             }
+
         });
     }
 }
