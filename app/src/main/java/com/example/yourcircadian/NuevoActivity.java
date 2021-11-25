243a726facbdb91b8342b470347a9247128c2772
Carlos Anton <cantonplk@gmail.com>
2021-11-25 14:56:33 +0000

    updates

--- app/src/main/java/com/example/yourcircadian/NuevoActivity.java
+++ app/src/main/java/com/example/yourcircadian/NuevoActivity.java
@@ -3,12 +3,23 @@ package com.example.yourcircadian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NuevoActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtTelefono, txtCorreoElectronico;
+    Button btnGuarda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreoElectronico= findViewById(R.id.txtCorreoElectronico);
+        btnGuarda = findViewById(R.id.btnGuarda);
+
     }
 }
