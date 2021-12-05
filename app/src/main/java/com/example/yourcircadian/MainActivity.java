1d0b5c96547f72c859224a5111eebd3e3d5a1869
Carlos Anton <cantonplk@gmail.com>
2021-12-05 19:45:57 +0000

    dsaf

--- app/src/main/java/com/example/yourcircadian/MainActivity.java
+++ app/src/main/java/com/example/yourcircadian/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.yourcircadian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Prueba prueba = new Prueba();
+        prueba.cambiarTexto(textView1);
     }
 }
