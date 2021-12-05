2cbd959c16651d622a98064544fdd15b6c5ff52f
Carlos Anton <cantonplk@gmail.com>
2021-12-05 19:45:57 +0000

    dsaf

---
+++ app/src/main/java/com/example/yourcircadian/Prueba.java
@@ -0,0 +1,12 @@
+package com.example.yourcircadian;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class Prueba {
+    public void cambiarTexto(View v){
+        TextView textView1;
+        textView1 = (TextView) v.findViewById(textView1);
+        textView1.setText("euyyyyyyy");
+    }
+}
