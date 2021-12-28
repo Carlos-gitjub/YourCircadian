1f2d29ebbca4c0d1e3d9121bf65672a6501cdec1
Carlos Anton <cantonplk@gmail.com>
2021-12-28 17:12:16 +0000

    first commit

--- app/src/main/java/com/example/yourcircadian/PhoneChargerConnectedListener.java
+++ app/src/main/java/com/example/yourcircadian/PhoneChargerConnectedListener.java
@@ -30,11 +30,11 @@ public class PhoneChargerConnectedListener extends BroadcastReceiver {
         if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
             //textView1.setText("Conectado");
             textView1.setText(String.valueOf(currentTime));          //Mon Dec 27 16:09:45 GMT+00:00 2021
-            long id = dbRegistros.insertarRegistro("algo");
+            long id = dbRegistros.insertarRegistro("Conectado " + String.valueOf(currentTime));
         } else if (Intent.ACTION_POWER_DISCONNECTED.equals(action)) {
             //textView1.setText("Desconectado");
             textView1.setText(String.valueOf(currentTime));
-            long id = dbRegistros.insertarRegistro("algo");
+            long id = dbRegistros.insertarRegistro("Desconectado " + String.valueOf(currentTime));
         }
     }
 }
