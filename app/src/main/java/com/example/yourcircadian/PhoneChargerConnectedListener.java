2c5918e4477d0821d087c02214d117f97d5f9101
Carlos Anton <cantonplk@gmail.com>
2021-12-30 02:11:10 +0000

    updates

--- app/src/main/java/com/example/yourcircadian/PhoneChargerConnectedListener.java
+++ app/src/main/java/com/example/yourcircadian/PhoneChargerConnectedListener.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +27,8 @@ public class PhoneChargerConnectedListener extends BroadcastReceiver {
         DbRegistros dbRegistros = new DbRegistros(context); //por si aca: .getApplicationContext()
 
         Date currentTime = Calendar.getInstance().getTime();
+        String registro = String.valueOf(currentTime);
+        separarStringFechaHora(registro); //lo separa en dos strings(fecha y hora)
 
         if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
             //textView1.setText("Conectado");
@@ -36,5 +39,10 @@ public class PhoneChargerConnectedListener extends BroadcastReceiver {
             textView1.setText(String.valueOf(currentTime));
             long id = dbRegistros.insertarRegistro("Desconectado " + String.valueOf(currentTime));
         }
+    }
+
+    public void separarStringFechaHora(String registro){
+       String hora = registro.substring(11,19);
+       Log.v("PRUEBA", hora);
     }
 }
