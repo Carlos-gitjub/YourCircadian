81725789207e970a169629535a1ac8d2f0bf205a
Carlos Anton <cantonplk@gmail.com>
2022-03-03 15:42:42 +0000

    updates

---
+++ app/src/main/java/com/example/yourcircadian/entidades/Weekday.java
@@ -0,0 +1,16 @@
+package com.example.yourcircadian.entidades;
+
+public class Weekday {
+    private String fecha;
+    private String weekday;
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) { this.fecha = fecha; }
+
+    public String getDiaSemana() { return weekday; }
+
+    public void setDiaSemana(String weekday) { this.weekday = weekday; }
+}
