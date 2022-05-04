05f059e8b4c20b91d50a18e96e61bfd54d0a9d72
Carlos-gitjub <cantonplk@gmail.com>
2022-05-04 09:39:42 +0100

    updates

--- app/src/main/java/com/example/yourcircadian/ManualActivity.java
+++ app/src/main/java/com/example/yourcircadian/ManualActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.yourcircadian.db.DbRegistros;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class ManualActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
+
+        DbRegistros dbRegistros = new DbRegistros(this);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String registro = String.valueOf(currentTime);
+
+        PhoneChargerConnectedListener phoneChargerConnectedListener = new PhoneChargerConnectedListener();
+
+        String fecha = phoneChargerConnectedListener.parserFecha(registro);
+        String hora = phoneChargerConnectedListener.parserHora(registro);
+
     }
 }
