115878257c53bb52fcfd11be8efe819d3eaecba3
Carlos-gitjub <cantonplk@gmail.com>
2022-05-04 11:28:13 +0100

    updates

--- app/src/main/java/com/example/yourcircadian/ManualActivity.java
+++ app/src/main/java/com/example/yourcircadian/ManualActivity.java
@@ -3,6 +3,8 @@ package com.example.yourcircadian;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.yourcircadian.db.DbRegistros;
 
@@ -16,8 +18,9 @@ public class ManualActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
 
+    }
+    public void comienza(View view) {
         DbRegistros dbRegistros = new DbRegistros(this);
-
         Date currentTime = Calendar.getInstance().getTime();
         String registro = String.valueOf(currentTime);
 
@@ -25,6 +28,21 @@ public class ManualActivity extends AppCompatActivity {
 
         String fecha = phoneChargerConnectedListener.parserFecha(registro);
         String hora = phoneChargerConnectedListener.parserHora(registro);
+        dbRegistros.insertarRegistro(fecha,hora,"Conexion");
 
+        Toast.makeText(ManualActivity.this,"Buenas noches",Toast.LENGTH_SHORT).show();
+    }
+    public void finaliza(View view){
+        DbRegistros dbRegistros = new DbRegistros(this);
+        Date currentTime = Calendar.getInstance().getTime();
+        String registro = String.valueOf(currentTime);
+
+        PhoneChargerConnectedListener phoneChargerConnectedListener = new PhoneChargerConnectedListener();
+
+        String fecha = phoneChargerConnectedListener.parserFecha(registro);
+        String hora = phoneChargerConnectedListener.parserHora(registro);
+        dbRegistros.insertarRegistro(fecha,hora,"Desconexion");
+
+        Toast.makeText(ManualActivity.this,"Buenos días",Toast.LENGTH_SHORT).show();
     }
 }
